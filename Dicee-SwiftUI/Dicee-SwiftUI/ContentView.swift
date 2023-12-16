e7772071507cee291e8f688bc7b85ec1f3b309f0
Mario Alvarado <mario.alvarado.isw@gmail.com>
2023-12-15 22:10:40 -0600

    ADD Dicee-SwiftUI

---
+++ Dicee-SwiftUI/Dicee-SwiftUI/ContentView.swift
@@ -0,0 +1,54 @@
+//
+//  ContentView.swift
+//  Dicee-SwiftUI
+//
+//  Created by Mario Alvarado on 15/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Image("background")
+                .resizable()
+                .ignoresSafeArea()
+            VStack {
+                Image("diceeLogo")
+                Spacer()
+                HStack {
+                    DiceView(n: 1)
+                    DiceView(n: 2)
+                }
+                .padding(.horizontal)
+                Spacer()
+                Button(action: {
+                    
+                }, label: {
+                    Text("Roll")
+                        .font(.system(size: 50))
+                        .fontWeight(.heavy)
+                        .foregroundStyle(.white)
+                        .padding(.horizontal)
+                })
+                .background(Color.red)
+            }
+        }
+    }
+}
+
+struct DiceView: View {
+    
+    let n: Int
+    
+    var body: some View {
+        Image("dice\(n)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
