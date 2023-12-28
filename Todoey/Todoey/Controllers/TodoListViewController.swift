8fd2f4b2a9f30a7dcfc2f76074fe427015ffe636
Mario Alvarado <mario.alvarado.isw@gmail.com>
2023-12-28 07:33:13 -0600

    ADD Decoding data from plist

--- Todoey/Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Todoey/Controllers/TodoListViewController.swift
@@ -16,21 +16,7 @@ class TodoListViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let newItem = Item()
-        newItem.title = "Have breakfast"
-        itemArray.append(newItem)
-        
-        let newItem2 = Item()
-        newItem2.title = "Make a coffee"
-        itemArray.append(newItem2)
-        
-        let newItem3 = Item()
-        newItem3.title = "Meditate"
-        itemArray.append(newItem3)
-        
-//        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
-//            itemArray = items
-//        }
+        loadItems()
     }
     
     
@@ -109,5 +95,16 @@ class TodoListViewController: UITableViewController {
         }
         
         self.tableView.reloadData()
+    }
+    
+    func loadItems() {
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("Error decoding item array, \(error)")
+            }
+        }
     }
 }
